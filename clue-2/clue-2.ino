e7a212205a42dd47464aad9756d5928fb6d02727
matt venn <matt@mattvenn.net>
2015-09-24 18:07:03 +0100

    formatting

--- clue-2/clue-2.ino
+++ clue-2/clue-2.ino
@@ -4,61 +4,62 @@
 #include <FS.h>
 
 const char *ssid = "First-Clue";
-ESP8266WebServer server ( 800 );
+ESP8266WebServer server(800);
 
-void setup ( void ) {
+void setup ()
+{
     WiFi.softAP(ssid);
 	IPAddress myIP = WiFi.softAPIP();
     Serial.begin(9600);
 	Serial.print("AP IP address: ");
 	Serial.println(myIP);
 
-	server.on ( "/", handleDownload );
+	server.on("/file", handleDownload);
+	server.onNotFound(handleNotFound);
 	server.begin();
-	Serial.println ( "HTTP server started" );
+	Serial.println("HTTP server started");
 }
 
-void loop ( void ) {
+void loop()
+{
 	server.handleClient();
 }
 
-void handleDownload(){
-  if (!SPIFFS.begin()) {
-    Serial.println("SPIFFS failed to mount !\r\n");                    
-  }
-  else {
-    String str = "";
-    File f = SPIFFS.open(server.arg(0), "r");
-    if (!f) {
-      Serial.println(server.arg(0));
-      Serial.println("Can't open SPIFFS file !\r\n");          
-    }
-
-  String dataType="text/plain";
-  if (server.hasArg("download")) dataType = "application/octet-stream";
-
-  if (server.streamFile(f, dataType) != f.size()) {
-    Serial.println("Sent less data than expected!");
-  }
-
-  f.close();
-  /*
-    else {
-      char buf[1024];
-      int siz = f.size();
-      while(siz > 0) {
-        Serial.println(siz);
-        size_t len = std::min((int)(sizeof(buf) - 1), siz);
-        f.read((uint8_t *)buf, len);
-        buf[len] = 0;
-        str += buf;
-        siz -= sizeof(buf) - 1;
-      }
-      f.close();
-      server.send(200, "text/plain", str);
-    }
-  }
-  */
+void handleNotFound() 
+{
+	String message = "<html>no such file</html>\n\n";
+	server.send(404, "text/html", message);
 }
+
+void handleDownload()
+{
+    if(!SPIFFS.begin()) 
+    {
+        Serial.println("SPIFFS failed to mount!");                    
+        server.send(500, "text/plain", "spiffs failed");
+        return;
+    }
+
+    if(!server.hasArg("file"))
+    {
+        Serial.println("need file parameter");                    
+        server.send(500, "text/plain", "need file parameter");
+        return;
+    }
+        
+    File f = SPIFFS.open(server.arg("file"), "r");
+    if(!f) 
+    {
+        Serial.println(server.arg(0));
+        Serial.println("Can't open SPIFFS file !\r\n");          
+        server.send(404, "text/plain", "no such file");
+        return;
+    }
+
+    if(server.streamFile(f, "text/plain") != f.size())
+    {
+        Serial.println("Sent less data than expected!");
+    }
+    f.close();
 }
 
